6eb5fe4c1f8cc3eb908c5733c5119e922d666116
FocusZero <liuzhanjing@gmail.com>
2016-05-11 21:37:55 +0800

    initial commit

---
+++ app/src/main/java/com/example/l/fragment/FragmentMainActivity.java
@@ -0,0 +1,24 @@
+package com.example.l.fragment;
+
+import android.app.FragmentManager;
+import android.hardware.fingerprint.FingerprintManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.app.Activity;
+
+public class FragmentMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_main);
+
+
+    }
+
+
+
+}
